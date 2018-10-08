ceph/ceph

librbd: use the correct error code when the exclusive lock isn't locked
If the client is currently blacklisted, use -EBLACKLISTED, otherwise
use -EROFS.

Signed-off-by: Jason Dillaman <dillaman@redhat.com>

src/librbd/deep_copy/ObjectCopyRequest.h
@@ -176,7 +176,7 @@ private:
   void send_update_object_map();
   void handle_update_object_map(int r);
 
-  Context *start_lock_op(RWLock &owner_lock);
+  Context *start_lock_op(RWLock &owner_lock, int* r);
 
   uint64_t src_to_dst_object_offset(uint64_t objectno, uint64_t offset);
 

src/librbd/deep_copy/SnapshotCopyRequest.cc
@@ -174,10 +174,11 @@ void SnapshotCopyRequest<I>::send_snap_unprotect() {
   ldout(m_cct, 20) << "snap_name=" << m_snap_name << ", "
                    << "snap_id=" << m_prev_snap_id << dendl;
 
-  auto finish_op_ctx = start_lock_op();
+  int r;
+  auto finish_op_ctx = start_lock_op(&r);
   if (finish_op_ctx == nullptr) {
     lderr(m_cct) << "lost exclusive lock" << dendl;
-    finish(-EROFS);
+    finish(r);
     return;
   }
 
@@ -270,10 +271,11 @@ void SnapshotCopyRequest<I>::send_snap_remove() {
            << "snap_name=" << m_snap_name << ", "
            << "snap_id=" << m_prev_snap_id << dendl;
 
-  auto finish_op_ctx = start_lock_op();
+  int r;
+  auto finish_op_ctx = start_lock_op(&r);
   if (finish_op_ctx == nullptr) {
     lderr(m_cct) << "lost exclusive lock" << dendl;
-    finish(-EROFS);
+    finish(r);
     return;
   }
 
@@ -370,10 +372,11 @@ void SnapshotCopyRequest<I>::send_snap_create() {
                    << "snap_id=" << parent_spec.snap_id << ", "
                    << "overlap=" << parent_overlap << "]" << dendl;
 
-  Context *finish_op_ctx = start_lock_op();
+  int r;
+  Context *finish_op_ctx = start_lock_op(&r);
   if (finish_op_ctx == nullptr) {
     lderr(m_cct) << "lost exclusive lock" << dendl;
-    finish(-EROFS);
+    finish(r);
     return;
   }
 
@@ -477,10 +480,11 @@ void SnapshotCopyRequest<I>::send_snap_protect() {
   ldout(m_cct, 20) << "snap_name=" << m_snap_name << ", "
                    << "snap_id=" << m_prev_snap_id << dendl;
 
-  auto finish_op_ctx = start_lock_op();
+  int r;
+  auto finish_op_ctx = start_lock_op(&r);
   if (finish_op_ctx == nullptr) {
     lderr(m_cct) << "lost exclusive lock" << dendl;
-    finish(-EROFS);
+    finish(r);
     return;
   }
 
@@ -571,7 +575,7 @@ void SnapshotCopyRequest<I>::send_resize_object_map() {
 
       ldout(m_cct, 20) << dendl;
 
-      auto finish_op_ctx = start_lock_op(m_dst_image_ctx->owner_lock);
+      auto finish_op_ctx = start_lock_op(m_dst_image_ctx->owner_lock, &r);
       if (finish_op_ctx != nullptr) {
         auto ctx = new FunctionContext([this, finish_op_ctx](int r) {
             handle_resize_object_map(r);
@@ -584,7 +588,6 @@ void SnapshotCopyRequest<I>::send_resize_object_map() {
       }
 
       lderr(m_cct) << "lost exclusive lock" << dendl;
-      r = -EROFS;
     }
   }
 
@@ -644,18 +647,18 @@ int SnapshotCopyRequest<I>::validate_parent(I *image_ctx,
 }
 
 template <typename I>
-Context *SnapshotCopyRequest<I>::start_lock_op() {
+Context *SnapshotCopyRequest<I>::start_lock_op(int* r) {
   RWLock::RLocker owner_locker(m_dst_image_ctx->owner_lock);
-  return start_lock_op(m_dst_image_ctx->owner_lock);
+  return start_lock_op(m_dst_image_ctx->owner_lock, r);
 }
 
 template <typename I>
-Context *SnapshotCopyRequest<I>::start_lock_op(RWLock &owner_lock) {
+Context *SnapshotCopyRequest<I>::start_lock_op(RWLock &owner_lock, int* r) {
   ceph_assert(m_dst_image_ctx->owner_lock.is_locked());
   if (m_dst_image_ctx->exclusive_lock == nullptr) {
     return new FunctionContext([](int r) {});
   }
-  return m_dst_image_ctx->exclusive_lock->start_op();
+  return m_dst_image_ctx->exclusive_lock->start_op(r);
 }
 
 template <typename I>

src/librbd/deep_copy/SnapshotCreateRequest.h
@@ -82,7 +82,7 @@ private:
   void send_create_object_map();
   void handle_create_object_map(int r);
 
-  Context *start_lock_op();
+  Context *start_lock_op(int* r);
 
   void finish(int r);
 };

A10506/ceph-ci

(cherry picked from commit e8eee15518facf562adf1aaba02d3a9523cdd2c3)
Conflicts:
	src/librbd/ExclusiveLock.cc: trivial resolution
	src/librbd/image/RemoveRequest.cc: trivial resolution
	src/test/rbd_mirror/image_deleter/test_mock_SnapshotPurgeRequest.cc: DNE
	src/tools/rbd_mirror/image_deleter/SnapshotPurgeRequest.cc: DNE
	src/tools/rbd_mirror/image_deleter/SnapshotPurgeRequest.h: DNE
	src/librbd/DeepCopyRequest.cc: (see below)
	src/librbd/deep_copy/ObjectCopyRequest.cc: (see below)
	src/librbd/deep_copy/ObjectCopyRequest.h: (see below)
	src/librbd/deep_copy/SetHeadRequest.cc: (see below)
	src/librbd/deep_copy/SetHeadRequest.h: (see below)
	src/librbd/deep_copy/SnapshotCopyRequest.cc: (see below)
	src/librbd/deep_copy/SnapshotCopyRequest.h: (see below)
	src/librbd/deep_copy/SnapshotCreateRequest.cc: (see below)
	src/librbd/deep_copy/SnapshotCreateRequest.h: (see below)
	src/test/librbd/deep_copy/test_mock_ObjectCopyRequest.cc: (see below)
	src/test/librbd/deep_copy/test_mock_SetHeadRequest.cc: (see below)
	src/test/librbd/deep_copy/test_mock_SnapshotCopyRequest.cc: (see below)
	src/test/librbd/deep_copy/test_mock_SnapshotCreateRequest.cc: (see below)
	src/test/librbd/test_mock_DeepCopyRequest.cc
- deep-copy related files were originally derived from rbd-mirror
  equivalents. Similar modifications where made to the associated
  rbd-mirror files.

src/tools/rbd_mirror/image_sync/ObjectCopyRequest.h
@@ -138,7 +138,7 @@ private:
   void send_update_object_map();
   void handle_update_object_map(int r);
 
-  Context *start_local_op(RWLock &owner_lock);
+  Context *start_local_op(RWLock &owner_lock, int *r);
 
   void compute_diffs();
   void finish(int r);

src/tools/rbd_mirror/image_sync/SnapshotCopyRequest.cc
@@ -170,10 +170,11 @@ void SnapshotCopyRequest<I>::send_snap_unprotect() {
            << "snap_name=" << m_snap_name << ", "
            << "snap_id=" << m_prev_snap_id << dendl;
 
-  auto finish_op_ctx = start_local_op();
+  int r;
+  auto finish_op_ctx = start_local_op(&r);
   if (finish_op_ctx == nullptr) {
     derr << ": lost exclusive lock" << dendl;
-    finish(-EROFS);
+    finish(r);
     return;
   }
 
@@ -257,10 +258,11 @@ void SnapshotCopyRequest<I>::send_snap_remove() {
            << "snap_name=" << m_snap_name << ", "
            << "snap_id=" << m_prev_snap_id << dendl;
 
-  auto finish_op_ctx = start_local_op();
+  int r;
+  auto finish_op_ctx = start_local_op(&r);
   if (finish_op_ctx == nullptr) {
     derr << ": lost exclusive lock" << dendl;
-    finish(-EROFS);
+    finish(r);
     return;
   }
 
@@ -360,10 +362,11 @@ void SnapshotCopyRequest<I>::send_snap_create() {
            << "snap_id=" << parent_spec.snap_id << ", "
            << "overlap=" << parent_overlap << "]" << dendl;
 
-  Context *finish_op_ctx = start_local_op();
+  int r;
+  Context *finish_op_ctx = start_local_op(&r);
   if (finish_op_ctx == nullptr) {
     derr << ": lost exclusive lock" << dendl;
-    finish(-EROFS);
+    finish(r);
     return;
   }
 
@@ -471,10 +474,11 @@ void SnapshotCopyRequest<I>::send_snap_protect() {
            << "snap_name=" << m_snap_name << ", "
            << "snap_id=" << m_prev_snap_id << dendl;
 
-  auto finish_op_ctx = start_local_op();
+  int r;
+  auto finish_op_ctx = start_local_op(&r);
   if (finish_op_ctx == nullptr) {
     derr << ": lost exclusive lock" << dendl;
-    finish(-EROFS);
+    finish(r);
     return;
   }
 
@@ -599,12 +603,13 @@ int SnapshotCopyRequest<I>::validate_parent(I *image_ctx,
 }
 
 template <typename I>
-Context *SnapshotCopyRequest<I>::start_local_op() {
+Context *SnapshotCopyRequest<I>::start_local_op(int *r) {
   RWLock::RLocker owner_locker(m_local_image_ctx->owner_lock);
   if (m_local_image_ctx->exclusive_lock == nullptr) {
+    *r = -EROFS;
     return nullptr;
   }
-  return m_local_image_ctx->exclusive_lock->start_op();
+  return m_local_image_ctx->exclusive_lock->start_op(r);
 }
 
 } // namespace image_sync

src/tools/rbd_mirror/image_sync/SnapshotCreateRequest.h
@@ -94,7 +94,7 @@ private:
   void send_create_object_map();
   void handle_create_object_map(int r);
 
-  Context *start_local_op();
+  Context *start_local_op(int *r);
 
   void finish(int r);
 };