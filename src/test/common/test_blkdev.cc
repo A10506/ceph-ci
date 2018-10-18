ceph/ceph

blkdev: overhaul API for better portability
* Turn the API into a class so it will work with GoogleMock
* Take file descriptors for all methods, instead of a mix of file
  descriptors, path names, and canonical device names.  It's more
  consistent and it will work better with FreeBSD.
* Split get_device_by_fd into block_device_partition and
  block_device_wholedisk
* Remove dead code

Signed-off-by: Alan Somers <asomers@gmail.com>

src/test/common/test_blkdev.cc
@@ -4,108 +4,90 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <linux/kdev_t.h>
 
 #include "include/types.h"
 #include "common/blkdev.h"
 
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include <iostream>
 
-  using namespace std;
+using namespace std;
+using namespace testing;
 
-TEST(blkdev, get_block_device_base) {
-  char buf[PATH_MAX*2];
-  char buf3[PATH_MAX*2];
+class MockBlkDev : public BlkDev {
+ public:
+  MockBlkDev() : BlkDev(-1) {};
+  virtual ~MockBlkDev() {}
 
-  ASSERT_EQ(-EINVAL, get_block_device_base("/etc/notindev", buf, 100));
+  MOCK_METHOD0(sysfsdir, const char*());
+  MOCK_METHOD2(block_device_wholedisk, int(char* device, size_t max));
+};
 
-  for (int i=0; i<2; ++i) {
-    string root;
-    if (i == 0) {
-      const char* env = getenv("CEPH_ROOT");
-      ASSERT_NE(env, nullptr) << "Environment Variable CEPH_ROOT not found!";
-      root = string(env) + "/src/test/common/test_blkdev_sys_block";
-    }
-    set_block_device_sandbox_dir(root.c_str());
 
-    // work backwards
-    sprintf(buf, "%s/sys/block", root.c_str());
-    DIR *dir = opendir(buf);
-    ASSERT_NE(dir, nullptr);
-    struct dirent *de = nullptr;
-    while ((de = ::readdir(dir))) {
-      if (de->d_name[0] == '.')
-	continue;
+class BlockDevTest : public ::testing::Test {
+public:
 
-      char base[PATH_MAX];
-      sprintf(base, "/dev/%s", de->d_name);
-      printf("base %s (%s)\n", base, de->d_name);
-      for (char *p = base; *p; ++p)
-	if (*p == '!')
-	  *p = '/';
+  string *root;
 
-      ASSERT_EQ(-ERANGE, get_block_device_base(base, buf3, 1));
-      ASSERT_EQ(0, get_block_device_base(base, buf3, sizeof(buf3)));
-      printf("  got '%s' expected '%s'\n", buf3, de->d_name);
-      ASSERT_EQ(0, strcmp(de->d_name, buf3));
-      printf("  discard  supported = %d\n",
-	     (int)block_device_support_discard(base));
+protected:
+  virtual void SetUp() {
+    const char *sda_name = "sda";
+    const char *sdb_name = "sdb";
+    const char* env = getenv("CEPH_ROOT");
+    ASSERT_NE(env, nullptr) << "Environment Variable CEPH_ROOT not found!";
+    root = new string(env);
+    *root += "/src/test/common/test_blkdev_sys_block/sys";
 
-      char subdirfn[PATH_MAX];
-      sprintf(subdirfn, "%s/sys/block/%s", root.c_str(), de->d_name);
-      DIR *subdir = opendir(subdirfn);
-      ASSERT_TRUE(subdir);
-      struct dirent *de2 = nullptr;
-      while ((de2 = ::readdir(subdir))) {
-	if (de2->d_name[0] == '.')
-	  continue;
-	// partiions will be prefixed with the base name
-	if (strncmp(de2->d_name, de->d_name, strlen(de->d_name))) {
-	  //printf("skipping %s\n", de2->d_name);
-	  continue;
-	}
-	char part[PATH_MAX];
-	sprintf(part, "/dev/%s", de2->d_name);
-	for (char *p = part; *p; ++p)
-	  if (*p == '!')
-	    *p = '/';
-	printf(" part %s (%s %s)\n", part, de->d_name, de2->d_name);
+    EXPECT_CALL(sda, sysfsdir())
+      .WillRepeatedly(Return(root->c_str()));
+    EXPECT_CALL(sda, block_device_wholedisk(NotNull(), Ge(0ul)))
+      .WillRepeatedly(
+        DoAll(
+          SetArrayArgument<0>(sda_name, sda_name + strlen(sda_name) + 1),
+          Return(0)));
 
-	ASSERT_EQ(0, get_block_device_base(part, buf3, sizeof(buf3)));
-	printf("  got '%s' expected '%s'\n", buf3, de->d_name);
-	ASSERT_EQ(0, strcmp(buf3, de->d_name));
-	printf("  discard  supported = %d\n",
-	       (int)block_device_support_discard(part));
-      }
-
-      closedir(subdir);
-    }
-    closedir(dir);
+    EXPECT_CALL(sdb, sysfsdir())
+      .WillRepeatedly(Return(root->c_str()));
+    EXPECT_CALL(sdb, block_device_wholedisk(NotNull(), Ge(0ul)))
+      .WillRepeatedly(
+        DoAll(
+          SetArrayArgument<0>(sdb_name, sdb_name + strlen(sdb_name) + 1),
+          Return(0)));
   }
-}
 
-TEST(blkdev, device_model)
+  virtual void TearDown() {
+    delete root;
+  }
+
+  MockBlkDev sda, sdb;
+};
+
+TEST_F(BlockDevTest, device_model)
 {
-  const char* env = getenv("CEPH_ROOT");
-  ASSERT_NE(env, nullptr) << "Environment Variable CEPH_ROOT not found!";
-  string root = string(env) + "/src/test/common/test_blkdev_sys_block";
-  set_block_device_sandbox_dir(root.c_str());
-
   char model[1000] = {0};
-  int rc = block_device_model("sda", model, sizeof(model));
+  int rc = sda.block_device_model(model, sizeof(model));
   ASSERT_EQ(0, rc);
-
-  printf("model '%s'\n", model);
-  ASSERT_EQ(strcmp(model, "myfancymodel"), 0);
+  ASSERT_STREQ(model, "myfancymodel");
 }
 
-TEST(blkdev, is_rotational)
+TEST_F(BlockDevTest, discard)
 {
-  const char* env = getenv("CEPH_ROOT");
-  ASSERT_NE(env, nullptr) << "Environment Variable CEPH_ROOT not found!";
-  string root = string(env) + "/src/test/common/test_blkdev_sys_block";
-  set_block_device_sandbox_dir(root.c_str());
+  EXPECT_TRUE(sda.block_device_support_discard());
+  EXPECT_TRUE(sdb.block_device_support_discard());
+}
 
-  ASSERT_FALSE(block_device_is_rotational("sda"));
-  ASSERT_TRUE(block_device_is_rotational("sdb"));
+TEST_F(BlockDevTest, is_nvme)
+{
+  // It would be nice to have a positive NVME test too, but I don't have any
+  // examples for the canned data.
+  EXPECT_FALSE(sda.block_device_is_nvme());
+  EXPECT_FALSE(sdb.block_device_is_nvme());
+}
+
+TEST_F(BlockDevTest, is_rotational)
+{
+  EXPECT_FALSE(sda.block_device_is_rotational());
+  EXPECT_TRUE(sdb.block_device_is_rotational());
 }

src/test/test_get_blkdev_props.cc
@@ -17,12 +17,10 @@ int main(int argc, char **argv)
 	bool discard_support;
 	bool nvme;
 	bool rotational;
-	char device[BUFSIZE];
-	char partition[BUFSIZE];
 	char dev[BUFSIZE];
 	char model[BUFSIZE];
 	char serial[BUFSIZE];
-	char device[BUFSIZE];
+	char wholedisk[BUFSIZE];
 	char partition[BUFSIZE];
 
 	if (argc != 2) {
@@ -36,29 +34,31 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	if ((ret = get_block_device_size(fd, &size)) < 0) {
+	BlkDev blkdev(fd);
+
+	if ((ret = blkdev.get_block_device_size(&size)) < 0) {
 		fprintf(stderr, "get_block_device_size: %s\n", strerror(-ret));
 		return -1;
 	}
 
-	if ((ret = get_device_by_fd(fd, partition, device, BUFSIZE)) < 0) {
-		fprintf(stderr, "get_device_by_fd: %s\n", strerror(-ret));
-		return -1;
-	}
+	discard_support = blkdev.block_device_support_discard();
 
-	discard_support = block_device_support_discard(device);
+	nvme = blkdev.block_device_is_nvme();
 
-	nvme = block_device_is_nvme(device);
+	rotational = blkdev.block_device_is_rotational();
 
-	rotational = block_device_is_rotational(device);
-
-	if ((ret = block_device_dev(device, dev, BUFSIZE)) < 0) {
+	if ((ret = blkdev.block_device_dev(dev, BUFSIZE)) < 0) {
 		fprintf(stderr, "block_device_dev: %s\n", strerror(-ret));
 		return -1;
 	}
 
-	if ((ret = blkdev.get_device_by_fd(partition, device, BUFSIZE)) < 0) {
-		fprintf(stderr, "get_device_by_fd: %s\n", strerror(-ret));
+	if ((ret = blkdev.block_device_partition(partition, BUFSIZE)) < 0) {
+		fprintf(stderr, "block_device_partition: %s\n", strerror(-ret));
+		return -1;
+	}
+
+	if ((ret = blkdev.block_device_wholedisk(wholedisk, BUFSIZE)) < 0) {
+		fprintf(stderr, "block_device_wholedisk: %s\n", strerror(-ret));
 		return -1;
 	}
 
@@ -70,7 +70,7 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	ret = block_device_serial(device, serial, BUFSIZE);
+	ret = blkdev.block_device_serial(serial, BUFSIZE);
 	if (ret == -ENOENT) {
 		snprintf(serial, BUFSIZE, "Unknown");
 	} else if (ret < 0) {
@@ -84,7 +84,7 @@ int main(int argc, char **argv)
 	fprintf(stdout, "NVME:\t\t%s\n", nvme ? "yes" : "no");
 	fprintf(stdout, "Rotational:\t%s\n", rotational ? "yes" : "no");
 	fprintf(stdout, "Dev:\t\t%s\n", dev);
-	fprintf(stdout, "Device:\t\t%s\n", device);
+	fprintf(stdout, "Whole disk:\t%s\n", wholedisk);
 	fprintf(stdout, "Partition:\t%s\n", partition);
 	fprintf(stdout, "Model:\t\t%s\n", model);
 	fprintf(stdout, "Serial:\t\t%s\n", serial);

src/tools/rbd/action/Import.cc
@@ -824,7 +824,8 @@ static int do_import(librados::Rados &rados, librbd::RBD &rbd,
 
     if (!size) {
       int64_t bdev_size = 0;
-      r = get_block_device_size(fd, &bdev_size);
+      BlkDev blkdev(fd);
+      r = blkdev.get_block_device_size(&bdev_size);
       if (r < 0) {
         std::cerr << "rbd: unable to get size of file/block device"
                   << std::endl;

A10506/ceph-ci

src/test/common/test_blkdev.cc
@@ -4,108 +4,90 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <linux/kdev_t.h>
 
 #include "include/types.h"
 #include "common/blkdev.h"
 
 #include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include <iostream>
 
-  using namespace std;
+using namespace std;
+using namespace testing;
 
-TEST(blkdev, get_block_device_base) {
-  char buf[PATH_MAX*2];
-  char buf3[PATH_MAX*2];
+class MockBlkDev : public BlkDev {
+ public:
+  // pass 0 as fd, so it won't try to use the empty devname
+  MockBlkDev() : BlkDev(0) {};
+  virtual ~MockBlkDev() {}
 
-  ASSERT_EQ(-EINVAL, get_block_device_base("/etc/notindev", buf, 100));
+  MOCK_CONST_METHOD0(sysfsdir, const char*());
+  MOCK_CONST_METHOD2(wholedisk, int(char* device, size_t max));
+};
 
-  for (int i=0; i<2; ++i) {
-    string root;
-    if (i == 0) {
-      const char* env = getenv("CEPH_ROOT");
-      ASSERT_NE(env, nullptr) << "Environment Variable CEPH_ROOT not found!";
-      root = string(env) + "/src/test/common/test_blkdev_sys_block";
-    }
-    set_block_device_sandbox_dir(root.c_str());
 
-    // work backwards
-    sprintf(buf, "%s/sys/block", root.c_str());
-    DIR *dir = opendir(buf);
-    ASSERT_NE(dir, nullptr);
-    struct dirent *de = nullptr;
-    while ((de = ::readdir(dir))) {
-      if (de->d_name[0] == '.')
-	continue;
+class BlockDevTest : public ::testing::Test {
+public:
+  string *root;
 
-      char base[PATH_MAX];
-      sprintf(base, "/dev/%s", de->d_name);
-      printf("base %s (%s)\n", base, de->d_name);
-      for (char *p = base; *p; ++p)
-	if (*p == '!')
-	  *p = '/';
+protected:
+  virtual void SetUp() {
+    const char *sda_name = "sda";
+    const char *sdb_name = "sdb";
+    const char* env = getenv("CEPH_ROOT");
+    ASSERT_NE(env, nullptr) << "Environment Variable CEPH_ROOT not found!";
+    root = new string(env);
+    *root += "/src/test/common/test_blkdev_sys_block/sys";
 
-      ASSERT_EQ(-ERANGE, get_block_device_base(base, buf3, 1));
-      ASSERT_EQ(0, get_block_device_base(base, buf3, sizeof(buf3)));
-      printf("  got '%s' expected '%s'\n", buf3, de->d_name);
-      ASSERT_EQ(0, strcmp(de->d_name, buf3));
-      printf("  discard  supported = %d\n",
-	     (int)block_device_support_discard(base));
+    EXPECT_CALL(sda, sysfsdir())
+      .WillRepeatedly(Return(root->c_str()));
+    EXPECT_CALL(sda, wholedisk(NotNull(), Ge(0ul)))
+      .WillRepeatedly(
+        DoAll(
+          SetArrayArgument<0>(sda_name, sda_name + strlen(sda_name) + 1),
+          Return(0)));
 
-      char subdirfn[PATH_MAX];
-      sprintf(subdirfn, "%s/sys/block/%s", root.c_str(), de->d_name);
-      DIR *subdir = opendir(subdirfn);
-      ASSERT_TRUE(subdir);
-      struct dirent *de2 = nullptr;
-      while ((de2 = ::readdir(subdir))) {
-	if (de2->d_name[0] == '.')
-	  continue;
-	// partiions will be prefixed with the base name
-	if (strncmp(de2->d_name, de->d_name, strlen(de->d_name))) {
-	  //printf("skipping %s\n", de2->d_name);
-	  continue;
-	}
-	char part[PATH_MAX];
-	sprintf(part, "/dev/%s", de2->d_name);
-	for (char *p = part; *p; ++p)
-	  if (*p == '!')
-	    *p = '/';
-	printf(" part %s (%s %s)\n", part, de->d_name, de2->d_name);
-
-	ASSERT_EQ(0, get_block_device_base(part, buf3, sizeof(buf3)));
-	printf("  got '%s' expected '%s'\n", buf3, de->d_name);
-	ASSERT_EQ(0, strcmp(buf3, de->d_name));
-	printf("  discard  supported = %d\n",
-	       (int)block_device_support_discard(part));
-      }
-
-      closedir(subdir);
-    }
-    closedir(dir);
+    EXPECT_CALL(sdb, sysfsdir())
+      .WillRepeatedly(Return(root->c_str()));
+    EXPECT_CALL(sdb, wholedisk(NotNull(), Ge(0ul)))
+      .WillRepeatedly(
+        DoAll(
+          SetArrayArgument<0>(sdb_name, sdb_name + strlen(sdb_name) + 1),
+          Return(0)));
   }
-}
 
-TEST(blkdev, device_model)
+  virtual void TearDown() {
+    delete root;
+  }
+
+  MockBlkDev sda, sdb;
+};
+
+TEST_F(BlockDevTest, device_model)
 {
-  const char* env = getenv("CEPH_ROOT");
-  ASSERT_NE(env, nullptr) << "Environment Variable CEPH_ROOT not found!";
-  string root = string(env) + "/src/test/common/test_blkdev_sys_block";
-  set_block_device_sandbox_dir(root.c_str());
-
   char model[1000] = {0};
-  int rc = block_device_model("sda", model, sizeof(model));
+  int rc = sda.model(model, sizeof(model));
   ASSERT_EQ(0, rc);
-
-  printf("model '%s'\n", model);
-  ASSERT_EQ(strcmp(model, "myfancymodel"), 0);
+  ASSERT_STREQ(model, "myfancymodel");
 }
 
-TEST(blkdev, is_rotational)
+TEST_F(BlockDevTest, discard)
 {
-  const char* env = getenv("CEPH_ROOT");
-  ASSERT_NE(env, nullptr) << "Environment Variable CEPH_ROOT not found!";
-  string root = string(env) + "/src/test/common/test_blkdev_sys_block";
-  set_block_device_sandbox_dir(root.c_str());
+  EXPECT_TRUE(sda.support_discard());
+  EXPECT_TRUE(sdb.support_discard());
+}
 
-  ASSERT_FALSE(block_device_is_rotational("sda"));
-  ASSERT_TRUE(block_device_is_rotational("sdb"));
+TEST_F(BlockDevTest, is_nvme)
+{
+  // It would be nice to have a positive NVME test too, but I don't have any
+  // examples for the canned data.
+  EXPECT_FALSE(sda.is_nvme());
+  EXPECT_FALSE(sdb.is_nvme());
+}
+
+TEST_F(BlockDevTest, is_rotational)
+{
+  EXPECT_FALSE(sda.is_rotational());
+  EXPECT_TRUE(sdb.is_rotational());
 }

src/test/test_get_blkdev_props.cc
@@ -20,12 +20,10 @@ int main(int argc, char **argv)
   bool discard_support;
   bool nvme;
   bool rotational;
-  char device[BUFSIZE];
-  char partition[BUFSIZE];
   char dev[BUFSIZE];
   char model[BUFSIZE];
   char serial[BUFSIZE];
-  char device[BUFSIZE];
+  char wholedisk[BUFSIZE];
   char partition[BUFSIZE];
 
   if (argc != 2) {
@@ -39,45 +37,47 @@ int main(int argc, char **argv)
     return -1;
   }
 
-  if ((ret = get_block_device_size(fd, &size)) < 0) {
-    fprintf(stderr, "get_block_device_size: %s\n", strerror(-ret));
+  BlkDev blkdev(fd);
+
+  if ((ret = blkdev.get_size(&size)) < 0) {
+    fprintf(stderr, "get_size: %s\n", strerror(-ret));
     return -1;
   }
 
-  if ((ret = get_device_by_fd(fd, partition, device, BUFSIZE)) < 0) {
-    fprintf(stderr, "get_device_by_fd: %s\n", strerror(-ret));
+  discard_support = blkdev.support_discard();
+
+  nvme = blkdev.is_nvme();
+
+  rotational = blkdev.is_rotational();
+
+  if ((ret = blkdev.dev(dev, BUFSIZE)) < 0) {
+    fprintf(stderr, "dev: %s\n", strerror(-ret));
     return -1;
   }
 
-  discard_support = block_device_support_discard(device);
-
-  nvme = block_device_is_nvme(device);
-
-  rotational = block_device_is_rotational(device);
-
-  if ((ret = block_device_dev(device, dev, BUFSIZE)) < 0) {
-    fprintf(stderr, "block_device_dev: %s\n", strerror(-ret));
+  if ((ret = blkdev.partition(partition, BUFSIZE)) < 0) {
+    fprintf(stderr, "partition: %s\n", strerror(-ret));
     return -1;
   }
 
-  if ((ret = blkdev.get_device_by_fd(partition, device, BUFSIZE)) < 0) {
-    fprintf(stderr, "get_device_by_fd: %s\n", strerror(-ret));
+  if ((ret = blkdev.wholedisk(wholedisk, BUFSIZE)) < 0) {
+    fprintf(stderr, "wholedisk: %s\n", strerror(-ret));
     return -1;
   }
 
-  ret = blkdev.block_device_model(model, BUFSIZE);
+  ret = blkdev.model(model, BUFSIZE);
   if (ret == -ENOENT) {
     snprintf(model, BUFSIZE, "Unknown");
   } else if (ret < 0) {
-    fprintf(stderr, "block_device_model: %s\n", strerror(-ret));
+    fprintf(stderr, "model: %s\n", strerror(-ret));
     return -1;
   }
 
-  ret = block_device_serial(device, serial, BUFSIZE);
+  ret = blkdev.serial(serial, BUFSIZE);
   if (ret == -ENOENT) {
     snprintf(serial, BUFSIZE, "Unknown");
   } else if (ret < 0) {
-    fprintf(stderr, "block_device_serial: %s\n", strerror(-ret));
+    fprintf(stderr, "serial: %s\n", strerror(-ret));
     return -1;
   }
 
@@ -87,7 +87,7 @@ int main(int argc, char **argv)
   fprintf(stdout, "NVME:\t\t%s\n", nvme ? "yes" : "no");
   fprintf(stdout, "Rotational:\t%s\n", rotational ? "yes" : "no");
   fprintf(stdout, "Dev:\t\t%s\n", dev);
-  fprintf(stdout, "Device:\t\t%s\n", device);
+  fprintf(stdout, "Whole disk:\t%s\n", wholedisk);
   fprintf(stdout, "Partition:\t%s\n", partition);
   fprintf(stdout, "Model:\t\t%s\n", model);
   fprintf(stdout, "Serial:\t\t%s\n", serial);

src/tools/rbd/action/Import.cc
@@ -868,7 +868,8 @@ static int do_import(librados::Rados &rados, librbd::RBD &rbd,
 
     if (!size) {
       int64_t bdev_size = 0;
-      r = get_block_device_size(fd, &bdev_size);
+      BlkDev blkdev(fd);
+      r = blkdev.get_size(&bdev_size);
       if (r < 0) {
         std::cerr << "rbd: unable to get size of file/block device"
                   << std::endl;